dba6c8c1b6d44d5239049f56f1b2fd84f0ed17c3
yanagiragi <qaz90473@gmail.com>
2019-11-19 21:00:05 +0800

    finish ch10

--- main.cpp
+++ main.cpp
@@ -29,6 +29,51 @@ vec3 color(const ray& r, hitable *world, int depth) {
     }    
 }
 
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+hitable *random_scene() {
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = random_double();
+            vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
+            if ((center-vec3(4,0.2,0)).length() > 0.9) {
+                if (choose_mat < 0.8) {  // diffuse
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new lambertian(vec3(random_double()*random_double(),
+                                            random_double()*random_double(),
+                                            random_double()*random_double()))
+                    );
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(
+                        center, 0.2,
+                        new metal(vec3(0.5*(1 + random_double()),
+                                       0.5*(1 + random_double()),
+                                       0.5*(1 + random_double()))
+                                  )
+                    );
+                }
+                else {  // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5)));
+
+    return new hitable_list(list,i);
+}
+
 int main(int argc, char**argv)
 {
     int nx = 200;
@@ -43,9 +88,15 @@ int main(int argc, char**argv)
     list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
     list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
 
-    hitable* world = new hitable_list(list, 5);
+    hitable *list2[5];
+    float R = cos(M_PI / 4);
+    list2[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)) );
+    list2[1] = new sphere(vec3(R, 0, -1), R, new lambertian(vec3(1, 0, 0)));    
 
-    camera cam;
+    hitable* world = new hitable_list(list, 5);
+    //hitable *world = random_scene();
+
+    camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 45, float(nx) / float(ny));
 
     for (int j = ny - 1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
